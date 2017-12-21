4b705218277680001c95ade32a108c175380bf61
alicjaszlachta <alaaszlachta@gmail.com>
2017-12-21 16:46:19 +0100

    Memetic algo added

--- evol/evol/src/memetic/Main.java
+++ evol/evol/src/memetic/Main.java
@@ -6,6 +6,7 @@ import jmetal.core.Algorithm;
 import jmetal.core.Operator;
 import jmetal.core.Problem;
 import jmetal.core.SolutionSet;
+import jmetal.metaheuristics.singleObjective.evolutionStrategy.ElitistES;
 import jmetal.metaheuristics.singleObjective.geneticAlgorithm.gGA;
 import jmetal.operators.crossover.CrossoverFactory;
 import jmetal.operators.localSearch.HillClimbing;
@@ -17,86 +18,65 @@ import jmetal.problems.singleObjective.Sphere;
 import jmetal.util.JMException;
 
 public class Main {
-  public static void main(String [] args) throws JMException, ClassNotFoundException {
-	    Problem   problem   ;         // The problem to solve
-	    Algorithm algorithm ;         // The algorithm to use
-	    Operator  crossover ;         // Crossover operator
-	    Operator  mutation  ;         // Mutation operator
-	    Operator  selection ;         // Selection operator
-	    Operator improvement; 		  // Operator for improvement
-	    
-	    //int bits ; // Length of bit string in the OneMax problem
-	    HashMap<String, Double>  parameters ; // Operator parameters
+	public static void main(String [] args) throws JMException, ClassNotFoundException {
+		Problem   problem   ;         // The problem to solve
+		Algorithm algorithm ;         // The algorithm to use
+		Operator  crossover ;         // Crossover operator
+		Operator  mutation  ;         // Mutation operator
+		Operator  selection ;         // Selection operator
+		Operator improvement; 		  // Operator for improvement
 
-	    int bits = 512;
-	    //problem = new OneMax("Binary", bits);
-	 
-//	    problem = new Sphere("Real", 100);
-	    //problem = new Easom("Real") ;
-	    //problem = new Griewank("Real", 10) ;
-	    problem = new Rastrigin("Real", 50);
-	    
-	    algorithm = new gGA(problem) ; // Generational GA
-	    //algorithm = new ssGA(problem); // Steady-state GA
-	    //algorithm = new scGA(problem) ; // Synchronous cGA
-	    //algorithm = new acGA(problem) ;   // Asynchronous cGA
-	    
+		HashMap<String, Double>  parameters ; // Operator parameters
+
+		problem = new Rastrigin("Real", 100);
+
+		algorithm = new MemeticAlgorithm(problem) ;
+
 	    /* Algorithm parameters*/
-	    algorithm.setInputParameter("populationSize", 1000);
-	    algorithm.setInputParameter("maxEvaluations", 25000);
-	    /*
-	    // Mutation and Crossover for Real codification 
-	    parameters = new HashMap() ;
-	    parameters.put("probability", 0.9) ;
-	    parameters.put("distributionIndex", 20.0) ;
-	    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);                   
+		algorithm.setInputParameter("populationSize", 100);
+		algorithm.setInputParameter("maxEvaluations", 25000);
 
-	    parameters = new HashMap() ;
-	    parameters.put("probability", 1.0/problem.getNumberOfVariables()) ;
-	    parameters.put("distributionIndex", 20.0) ;
-	    mutation = MutationFactory.getMutationOperator("PolynomialMutation", parameters);                    
-	    */
-	    
-	    // Mutation and Crossover for Binary codification 
-	    parameters = new HashMap<String, Double>() ;
-	    parameters.put("probability", 0.9) ;
-	    parameters.put("distributionIndex", 0.1) ;
-	    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);                   
+		// Mutation and Crossover for Binary codification
+		parameters = new HashMap<String, Double>() ;
+		parameters.put("probability", 0.9) ;
+		parameters.put("distributionIndex", 0.1) ;
+		crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);
 
-	    parameters = new HashMap<String, Double>() ;
-	    parameters.put("probability", 0.1) ;
-	    parameters.put("perturbation", 0.1) ;
-	    mutation = MutationFactory.getMutationOperator("UniformMutation", parameters);                    
-	    
+		parameters = new HashMap<String, Double>() ;
+		parameters.put("probability", 0.1) ;
+		parameters.put("perturbation", 0.1) ;
+		mutation = MutationFactory.getMutationOperator("UniformMutation", parameters);
+
 	    /* Selection Operator */
-	    parameters = null ;
-	    selection = SelectionFactory.getSelectionOperator("BinaryTournament", parameters) ;                            
-	    
-	    HashMap params = new HashMap() ;
-	    params.put("improvementRounds", 1) ;
-	    params.put("problem", problem) ;
-	    params.put("mutation", mutation) ;
-//	    improvement = new MutationLocalSearch(parameters);
-	    improvement = new HillClimbing(params);
+		parameters = null ;
+		selection = SelectionFactory.getSelectionOperator("BinaryTournament", parameters) ;
 
-	    // Adding the operators to the algorithm
-	    algorithm.addOperator("crossover", crossover);
-	    algorithm.addOperator("improvement", improvement);
+		HashMap params = new HashMap() ;
+		params.put("improvementRounds", 5) ;
+		params.put("problem", problem) ;
+		params.put("mutation", mutation) ;
+//	    params.put("max_local_gens", 20) ;
+		improvement = new MutationLocalSearch(params);
+//	    improvement = new HillClimbing(params);
+
+		// Adding the operators to the algorithm
+		algorithm.addOperator("crossover", crossover);
+		algorithm.addOperator("improvement", improvement);
 	    /* Add the operators to the algorithm*/
-	    algorithm.addOperator("crossover", crossover);
-	    algorithm.addOperator("mutation", mutation);
-	    algorithm.addOperator("selection", selection);
+		algorithm.addOperator("crossover", crossover);
+		algorithm.addOperator("mutation", mutation);
+		algorithm.addOperator("selection", selection);
 	 
 	    /* Execute the Algorithm */
-	    long initTime = System.currentTimeMillis();
-	    SolutionSet population = algorithm.execute();
-	    long estimatedTime = System.currentTimeMillis() - initTime;
-	    System.out.println("Total execution time: " + estimatedTime);
+		long initTime = System.currentTimeMillis();
+		SolutionSet population = algorithm.execute();
+		long estimatedTime = System.currentTimeMillis() - initTime;
+		System.out.println("Total execution time: " + estimatedTime);
 
 	    /* Log messages */
-	    System.out.println("Objectives values have been writen to file FUN");
-	    population.printObjectivesToFile("FUN");
-	    System.out.println("Variables values have been writen to file VAR");
-	    population.printVariablesToFile("VAR");          
-	  } //main
+		System.out.println("Objectives values have been writen to file FUN");
+		population.printObjectivesToFile("FUN");
+		System.out.println("Variables values have been writen to file VAR");
+		population.printVariablesToFile("VAR");
+	} //main
 }
---
+++ evol/evol/src/memetic/MemeticAlgorithm.java
@@ -0,0 +1,135 @@
+package memetic;
+
+import jmetal.core.*;
+import jmetal.operators.localSearch.LocalSearch;
+import jmetal.util.JMException;
+import jmetal.util.comparators.ObjectiveComparator;
+
+import java.util.Comparator;
+
+public class MemeticAlgorithm extends Algorithm{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+//	private int mu_;
+//	private int lambda_;
+
+    public MemeticAlgorithm(Problem problem) {
+        super(problem);
+    }
+
+    /**
+     * Execute the Memetic algorithm
+     *
+     * @throws JMException
+     */
+    public SolutionSet execute() throws JMException, ClassNotFoundException {
+        int populationSize;
+        int maxEvaluations;
+        int evaluations;
+
+        SolutionSet population;
+        SolutionSet offspringPopulation;
+
+        Operator mutationOperator;
+        Operator crossoverOperator;
+        Operator selectionOperator;
+        Operator localSearchOperator;
+
+        Comparator comparator;
+        comparator = new ObjectiveComparator(0); // Single objective comparator
+
+        // Read the params
+        populationSize = ((Integer) this.getInputParameter("populationSize")).intValue();
+        maxEvaluations = ((Integer) this.getInputParameter("maxEvaluations")).intValue();
+
+        // Initialize the variables
+        population = new SolutionSet(populationSize);
+        offspringPopulation = new SolutionSet(populationSize);
+
+        evaluations = 0;
+        // Read the operators
+        mutationOperator = this.operators_.get("mutation");
+        crossoverOperator = this.operators_.get("crossover");
+        selectionOperator = this.operators_.get("selection");
+        localSearchOperator = (LocalSearch) operators_.get("improvement");
+
+        // Create the initial population
+        Solution newIndividual;
+        for (int i = 0; i < populationSize; i++) {
+            newIndividual = new Solution(problem_);
+            problem_.evaluate(newIndividual);
+            evaluations++;
+            population.add(newIndividual);
+        } //for
+
+        // Sort population
+        population.sort(comparator);
+
+        while ((evaluations < maxEvaluations)) {
+            // if ((evaluations % 10) == 0) {
+            System.out.println(population.get(0).getObjective(0));
+            // } //
+
+            // Copy the best two individuals to the offspring population
+            offspringPopulation.add(new Solution(population.get(0)));
+//			offspringPopulation.add(new Solution(population.get(1)));
+
+            // Reproductive cycle
+            for (int i = 0; i < (populationSize - 1); i++) {
+                // Selection
+                Solution[] parents = new Solution[2];
+
+                parents[0] = (Solution) selectionOperator.execute(population);
+                parents[1] = (Solution) selectionOperator.execute(population);
+
+                // Crossover
+                Solution[] offspring = (Solution[]) crossoverOperator.execute(parents);
+
+                // Mutation
+                mutationOperator.execute(offspring[0]);
+                System.out.println("Offspring fitness " + offspring[0].getObjective(0));
+                System.out.println("parents[0] fitness " + parents[0].getObjective(0));
+                System.out.println("parents[1] fitness " + parents[1].getObjective(0));
+
+                // Evaluation of the new individual
+                problem_.evaluate(offspring[0]);
+
+                evaluations += 1;
+//
+                // Local Search using HillClimbing
+
+                offspring[0] = (Solution) localSearchOperator.execute(offspring[0]);
+
+                // Replacement: the two new individuals are inserted in the offspring
+                // population
+                if(parents[0].getFitness() > offspring[0].getFitness()) {
+                    offspringPopulation.add(parents[0]);
+                }else {
+                    offspringPopulation.add(offspring[0]);
+                }
+
+//				offspringPopulation.add(offspring[1]);
+            } // for
+
+
+
+
+            // The offspring population becomes the new current population
+            population.clear();
+            for (int i = 0; i < populationSize; i++) {
+                population.add(offspringPopulation.get(i));
+            }
+            offspringPopulation.clear();
+            population.sort(comparator);
+        } // while
+
+        // Return a population with the best individual
+        SolutionSet resultPopulation = new SolutionSet(1);
+        resultPopulation.add(population.get(0));
+
+        System.out.println("Evaluations: " + evaluations);
+        return resultPopulation;
+    } // execute
+}
